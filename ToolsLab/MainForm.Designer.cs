ae6d19ef5e2cba3daf9428300ce5759696f47770
xuxuzhaozhao <xuxuzhaozhao87@gmail.com>
2022-12-05 09:21:50 +0800

    modified font

--- ToolsLab/MainForm.Designer.cs
+++ ToolsLab/MainForm.Designer.cs
@@ -56,10 +56,11 @@ namespace ToolsLab
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Location = new System.Drawing.Point(18, 18);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(586, 418);
+            this.tabControl1.Size = new System.Drawing.Size(782, 557);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -81,19 +82,21 @@ namespace ToolsLab
             this.tabPage1.Controls.Add(this.label2);
             this.tabPage1.Controls.Add(this.txtMsgSubject);
             this.tabPage1.Controls.Add(this.label1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Location = new System.Drawing.Point(4, 26);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(578, 392);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(774, 527);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "伦理消息发送";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // btnTest
             // 
-            this.btnTest.Location = new System.Drawing.Point(408, 349);
+            this.btnTest.Location = new System.Drawing.Point(544, 480);
+            this.btnTest.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnTest.Name = "btnTest";
-            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.Size = new System.Drawing.Size(100, 30);
             this.btnTest.TabIndex = 15;
             this.btnTest.Text = "测试";
             this.btnTest.UseVisualStyleBackColor = true;
@@ -101,9 +104,10 @@ namespace ToolsLab
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(489, 349);
+            this.btnSend.Location = new System.Drawing.Point(652, 480);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.Size = new System.Drawing.Size(100, 30);
             this.btnSend.TabIndex = 15;
             this.btnSend.Text = "发送";
             this.btnSend.UseVisualStyleBackColor = true;
@@ -111,59 +115,66 @@ namespace ToolsLab
             // 
             // txtUserTaskID
             // 
-            this.txtUserTaskID.Location = new System.Drawing.Point(264, 105);
+            this.txtUserTaskID.Location = new System.Drawing.Point(352, 154);
+            this.txtUserTaskID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUserTaskID.Name = "txtUserTaskID";
-            this.txtUserTaskID.Size = new System.Drawing.Size(302, 21);
+            this.txtUserTaskID.Size = new System.Drawing.Size(402, 26);
             this.txtUserTaskID.TabIndex = 14;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(8, 110);
+            this.label6.Location = new System.Drawing.Point(10, 161);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(245, 12);
+            this.label6.Size = new System.Drawing.Size(328, 16);
             this.label6.TabIndex = 13;
             this.label6.Text = "本次申请流程伦理秘书审核后的UserTaskID：";
             // 
             // txtObjectID
             // 
-            this.txtObjectID.Location = new System.Drawing.Point(142, 3);
+            this.txtObjectID.Location = new System.Drawing.Point(190, 18);
+            this.txtObjectID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtObjectID.Name = "txtObjectID";
-            this.txtObjectID.Size = new System.Drawing.Size(422, 21);
+            this.txtObjectID.Size = new System.Drawing.Size(562, 26);
             this.txtObjectID.TabIndex = 12;
             this.txtObjectID.TextChanged += new System.EventHandler(this.txtObjectID_TextChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(10, 8);
+            this.label5.Location = new System.Drawing.Point(14, 25);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(125, 12);
+            this.label5.Size = new System.Drawing.Size(168, 16);
             this.label5.TabIndex = 11;
             this.label5.Text = "IRBRequestAnswerID：";
             // 
             // txtSendUserID
             // 
-            this.txtSendUserID.Location = new System.Drawing.Point(120, 70);
+            this.txtSendUserID.Location = new System.Drawing.Point(160, 107);
+            this.txtSendUserID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSendUserID.Name = "txtSendUserID";
-            this.txtSendUserID.Size = new System.Drawing.Size(446, 21);
+            this.txtSendUserID.Size = new System.Drawing.Size(594, 26);
             this.txtSendUserID.TabIndex = 10;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(8, 75);
+            this.label4.Location = new System.Drawing.Point(10, 114);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(101, 12);
+            this.label4.Size = new System.Drawing.Size(136, 16);
             this.label4.TabIndex = 9;
             this.label4.Text = "伦理秘书UserID：";
             // 
             // rdo7403
             // 
             this.rdo7403.AutoSize = true;
-            this.rdo7403.Location = new System.Drawing.Point(507, 38);
+            this.rdo7403.Location = new System.Drawing.Point(676, 65);
+            this.rdo7403.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdo7403.Name = "rdo7403";
-            this.rdo7403.Size = new System.Drawing.Size(59, 16);
+            this.rdo7403.Size = new System.Drawing.Size(74, 20);
             this.rdo7403.TabIndex = 8;
             this.rdo7403.TabStop = true;
             this.rdo7403.Text = "不同意";
@@ -173,9 +184,10 @@ namespace ToolsLab
             // rdo7402
             // 
             this.rdo7402.AutoSize = true;
-            this.rdo7402.Location = new System.Drawing.Point(245, 38);
+            this.rdo7402.Location = new System.Drawing.Point(326, 65);
+            this.rdo7402.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdo7402.Name = "rdo7402";
-            this.rdo7402.Size = new System.Drawing.Size(71, 16);
+            this.rdo7402.Size = new System.Drawing.Size(90, 20);
             this.rdo7402.TabIndex = 7;
             this.rdo7402.TabStop = true;
             this.rdo7402.Text = "快速审查";
@@ -185,9 +197,10 @@ namespace ToolsLab
             // rdo7400
             // 
             this.rdo7400.AutoSize = true;
-            this.rdo7400.Location = new System.Drawing.Point(382, 38);
+            this.rdo7400.Location = new System.Drawing.Point(510, 65);
+            this.rdo7400.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdo7400.Name = "rdo7400";
-            this.rdo7400.Size = new System.Drawing.Size(47, 16);
+            this.rdo7400.Size = new System.Drawing.Size(58, 20);
             this.rdo7400.TabIndex = 6;
             this.rdo7400.TabStop = true;
             this.rdo7400.Text = "备案";
@@ -197,9 +210,10 @@ namespace ToolsLab
             // rdo7401
             // 
             this.rdo7401.AutoSize = true;
-            this.rdo7401.Location = new System.Drawing.Point(120, 38);
+            this.rdo7401.Location = new System.Drawing.Point(160, 65);
+            this.rdo7401.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rdo7401.Name = "rdo7401";
-            this.rdo7401.Size = new System.Drawing.Size(71, 16);
+            this.rdo7401.Size = new System.Drawing.Size(90, 20);
             this.rdo7401.TabIndex = 5;
             this.rdo7401.TabStop = true;
             this.rdo7401.Text = "会议审查";
@@ -209,52 +223,59 @@ namespace ToolsLab
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 40);
+            this.label3.Location = new System.Drawing.Point(12, 67);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.Size = new System.Drawing.Size(88, 16);
             this.label3.TabIndex = 4;
-            this.label3.Text = "审核类型：";
+            this.label3.Text = "审查类型：";
             // 
             // rtxMsgText
             // 
-            this.rtxMsgText.Location = new System.Drawing.Point(77, 180);
+            this.rtxMsgText.Location = new System.Drawing.Point(102, 254);
+            this.rtxMsgText.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rtxMsgText.Name = "rtxMsgText";
-            this.rtxMsgText.Size = new System.Drawing.Size(487, 160);
+            this.rtxMsgText.Size = new System.Drawing.Size(648, 212);
             this.rtxMsgText.TabIndex = 3;
             this.rtxMsgText.Text = "";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(8, 180);
+            this.label2.Location = new System.Drawing.Point(10, 254);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.Size = new System.Drawing.Size(88, 16);
             this.label2.TabIndex = 2;
             this.label2.Text = "消息内容：";
             // 
             // txtMsgSubject
             // 
-            this.txtMsgSubject.Location = new System.Drawing.Point(79, 142);
+            this.txtMsgSubject.Location = new System.Drawing.Point(106, 203);
+            this.txtMsgSubject.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtMsgSubject.Name = "txtMsgSubject";
-            this.txtMsgSubject.Size = new System.Drawing.Size(487, 21);
+            this.txtMsgSubject.Size = new System.Drawing.Size(648, 26);
             this.txtMsgSubject.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(8, 146);
+            this.label1.Location = new System.Drawing.Point(10, 209);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.Size = new System.Drawing.Size(88, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "消息主题：";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(610, 440);
+            this.ClientSize = new System.Drawing.Size(814, 587);
             this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "伊柯夫-复旦肿瘤运维工具库";
